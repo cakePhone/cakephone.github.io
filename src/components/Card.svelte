ea5a0bac4c0e633ea92734bc8c7dd0544201b54a
cakePhone <ms8042939@gmail.com>
2023-10-03 20:36:44 +0100

    /imgs & /components -> /src. Using bun now

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,7 +8,7 @@
 	export let github;
 </script>
 
-<div class="card card-hover variant-filled-surface my-4 w-full overflow-hidden lg:m-4 lg:w-96">
+<div class="card card-hover variant-filled-surface my-4 w-full overflow-hidden lg:w-96 lg:my-0">
 	<img src={image} alt={title} class="h-fit object-cover lg:h-52" />
 	<header class="card-header" data-toc-ignore>{title}</header>
 	<section class="p-4">
