26934cf615d9d97b13ac4712d128bed9d179aac1
cakePhone <ms8042939@gmail.com>
2024-01-02 11:31:39 +0000

    Initial commit of rework

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,5 @@
 <script>
-	// The ordering of these imports is critical to your app working properly
-	import '../theme.postcss';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
-
-	function getImageURL(name) {
-		return new URL(`./imgs/${name}.jpg`, import.meta.url).href;
-	}
+  import "../app.postcss";
 </script>
-
-<svelte:head>
-	<!--Meta Tags for embeds-->
-	<meta property="og:image" content={getImageURL('embed')} />
-	<meta property="og:title" content="Miguel Santos' Portfolio" />
-	<meta property="og:description" content="An interesting Portfolio made by Miguel" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://cakephone.github.io/OreoPortfolio" />
-	<title>Miguel's Portfolio</title>
-</svelte:head>
 
 <slot />
