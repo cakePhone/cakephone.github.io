6de91d14e14f93f727393e26991d5426fa04be85
cakePhone <ms8042939@gmail.com>
2024-01-27 12:57:35 +0000

    Allow for Github Pages in config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
 import adapter from "@sveltejs/adapter-static";
 import sveltePreprocess from "svelte-preprocess";
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [sveltePreprocess({})],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+
+    paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
   },
 };
 
