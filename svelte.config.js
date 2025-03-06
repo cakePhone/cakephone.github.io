0e22a57356f3a85910717627c7a3fe7a83c05cd5
Miguel Santos <ms8042939@gmail.com>
2025-03-06 11:25:27 +0000

    enable inspector

--- svelte.config.js
+++ svelte.config.js
@@ -20,6 +20,13 @@ const config = {
       base: dev ? "" : process.env.BASE_PATH,
     },
   },
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: "alt-x",
+      showToggleButton: "always",
+      toggleButtonPos: "bottom-right",
+    },
+  },
 };
 
 export default config;
