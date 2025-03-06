abd5bb9cafbd563176b97230aa530589fbcea633
Miguel Santos <ms8042939@gmail.com>
2025-03-06 18:35:08 +0000

    everything should now work as expected

--- src/routes/blog/[title]/+page.ts
+++ src/routes/blog/[title]/+page.ts
@@ -1,19 +1,24 @@
 import type { EntryGenerator } from "./$types";
 
 export async function entries(): Promise<EntryGenerator> {
-  return [{ title: "about-me" }];
   let posts: Post[];
   try {
     posts = await (
       await fetch("http://cakephone.github.io/blog/posts.json")
     ).json();
   } catch (e) {
-    posts = await (await fetch("http://localhost:5173/blog/posts.json")).json();
+    try {
+      posts = await (
+        await fetch("http://localhost:5173/blog/posts.json")
+      ).json();
+    } catch (e) {
+      posts = [];
+    }
   }
 
-  // return posts.map((post) => ({
-  //   title: post.asset_path,
-  // }));
+  return posts.map((post) => ({
+    title: post.asset_path,
+  }));
 }
 
 export const prerender = true;
