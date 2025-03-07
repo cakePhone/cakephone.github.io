e36171fc8dfe3d31ae6900fa217ffa57e19e244f
Miguel Santos <ms8042939@gmail.com>
2025-03-07 10:12:22 +0000

    make breadcrumbs smaller on smaller screens

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -2,14 +2,20 @@
   import { page } from "$app/state";
 
   let { children } = $props();
+
+  let width: number = $state(0);
 </script>
+
+<svelte:window bind:outerWidth={width} />
 
 <header>
   <nav>
     <a href="/">Home</a>
     <a href="/blog">Blog</a>
   </nav>
-  <p>cakephone{page.url.pathname}</p>
+  <p>
+    {#if width > 800}cakephone{/if}{page.url.pathname}
+  </p>
 </header>
 
 <article>
